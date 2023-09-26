a2586134c5337d5345dad317e6832cb64b02c881
kapp1cka <simon.soltes07@gmail.com>
2023-09-26 14:52:07 +0200

    autoclick

--- res/js/main.js
+++ res/js/main.js
@@ -1,12 +1,14 @@
 const cookie = document.getElementById("cookie");
 const counter = document.getElementById("counter");
 const clickUpgrade = document.getElementById("clickUpgrade");
-
+const autoClicker = document.getElementById("autoClicker")
 
 let numberOfCookies = 0;
 let costOfClickUpgrade = 20;
 let clickUpgradeIncrease = 1;
-
+let autoclickIncrease = 0;
+let automatickeklikani;
+let autoclickerPrice = 100;
 cookie.onclick = () => {
     numberOfCookies+= clickUpgradeIncrease;
     //counter.innerHTML = "Cookies: " + numberOfCookies;
@@ -29,7 +31,29 @@ const clickUpgradeFunction = () => {
 
 clickUpgrade.onclick = clickUpgradeFunction;
 
+autoClicker.onclick = () => {
+    if (numberOfCookies >= autoclickerPrice) {
+        numberOfCookies -= autoclickerPrice;
+        //aktualizace poctu
+        counter.innerHTML = `Cookies: ${numberOfCookies}`;
+        //zvyseni ucinnosti
+        autoclickIncrease++;
+        //cena
+        autoclickerPrice *= 2;
+        autoClicker.innerText = "Buy slave: " + autoclickerPrice; 
+        //clear
+        clearInterval(automatickeklikani);
+        //interval
+        automatickeklikani = setInterval(() => {
+            numberOfCookies += autoclickIncrease;
+            counter.innerHTML = `Cookies: ${numberOfCookies}`;
+        }, 1000);
 
+    }
+
+
+
+}
 
 
 
@@ -50,3 +74,4 @@ const cheats2 = () => {
 }
 
 
+cheats();
