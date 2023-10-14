79ce568a010ea9a324bd1d76cc4bf6473790f16c
Kapp1cka <simon.soltes07@gmail.com>
2023-10-14 19:21:43 +0200

    idk

--- res/js/main.js
+++ res/js/main.js
@@ -4,6 +4,11 @@ const clickUpgrade = document.getElementById("clickUpgrade");
 const autoClicker = document.getElementById("autoClicker");
 const upgradeResource = document.getElementById("upgradeResource");
 const progressBar = document.getElementById("progressBar").getAttribute("aria-valuenow");
+const Otrokarna = document.getElementById("Otrokarna");
+const perSec = document.getElementById("perSec");
+const excavatorUpgrade = document.getElementById("excavatorUpgrade");
+
+
 
 let numberOfCookies = 0;
 let costOfClickUpgrade = 20;
@@ -14,6 +19,14 @@ let autoclickerPrice = 100;
 let upgradeCount = 0;
 let resourceUpgradePrice = 10;
 let resourcePrice = 2000;
+let lepsiOtrok = 0;
+let lepsiOtrokCena = 5000;
+let otrok;
+let costOfExcavator = 1000;
+let ExcavatorIncrease = 10;
+  
+
+
 
 upgradeResource.onclick = () => {
     if (upgradeCount >= resourceUpgradePrice && numberOfCookies >= resourcePrice) {
@@ -25,34 +38,35 @@ upgradeResource.onclick = () => {
             document.getElementById("cookie").src = "res/img/stone.jpeg";
             resetProgress();
         } 
-         if (document.getElementById("cookie").getAttribute("src") == "res/img/stone.jpeg") {
+        else if (document.getElementById("cookie").getAttribute("src") == "res/img/stone.jpeg") {
             document.getElementById("cookie").src = "res/img/copper.jpeg";
             resetProgress();
         } 
-         if (document.getElementById("cookie").getAttribute("src") == "res/img/copper.jpeg") {
+        else if (document.getElementById("cookie").getAttribute("src") == "res/img/copper.jpeg") {
             document.getElementById("cookie").src = "res/img/tin.jpeg";
             resetProgress();
         } 
-         if (document.getElementById("cookie").getAttribute("src") == "res/img/tin.jpeg") {
+        else if (document.getElementById("cookie").getAttribute("src") == "res/img/tin.jpeg") {
             document.getElementById("cookie").src = "res/img/tantalum.jpg";
             resetProgress();
         } 
-         if (document.getElementById("cookie").getAttribute("src") == "res/img/tantalum.jpg") {
+        else if (document.getElementById("cookie").getAttribute("src") == "res/img/tantalum.jpg") {
             document.getElementById("cookie").src = "res/img/lithium.jpeg";
             resetProgress();
         } 
-         if (document.getElementById("cookie").getAttribute("src") == "res/img/lithium.jpeg") {
+        else if (document.getElementById("cookie").getAttribute("src") == "res/img/lithium.jpeg") {
             document.getElementById("cookie").src = "res/img/cobalt.png";
             resetProgress();
         } 
-          if (document.getElementById("cookie").getAttribute("src") == "res/img/cobalt.jpeg") {
+        else  if (document.getElementById("cookie").getAttribute("src") == "res/img/cobalt.jpeg") {
             document.getElementById("cookie").src = "res/img/gold.jpeg";
             resetProgress();
         } 
-          if (document.getElementById("cookie").getAttribute("src") == "res/img/gold.jpeg") {
+        else  if (document.getElementById("cookie").getAttribute("src") == "res/img/gold.jpeg") {
             document.getElementById("cookie").src = "res/img/diamond.jpeg";
             resetProgress();
-        }  if (document.getElementById("cookie").getAttribute("src") == "res/img/diamond.jpeg"){
+        } 
+        else if (document.getElementById("cookie").getAttribute("src") == "res/img/diamond.jpeg"){
             upgradeResource.innerHTML = "MAXED";
             progressBar.innerHTML = " ";
            
@@ -65,10 +79,6 @@ upgradeResource.onclick = () => {
         alert("not enough money/resources");
     }
 }
-
-
-
-
 function pictureChange() {
     if (document.getElementById("shopIcon").getAttribute("src") == "/res/img/shopicon.png") {
         document.getElementById("shopIcon").src = "res/img/close.png";
@@ -76,6 +86,8 @@ function pictureChange() {
         document.getElementById("shopIcon").src = "/res/img/shopicon.png";
     }
 }
+
+
 
 document.getElementById("shopIcon").addEventListener('click', pictureChange);
 
@@ -93,7 +105,6 @@ let x = 100 / resourceUpgradePrice;
 let y =x
 function updateProgress() {
     const progressTxt = document.getElementById("progressTxt");
-
     console.log(x);
     progressBar == x;
     document.getElementById("progressBar").style.width = x + "%";
@@ -103,7 +114,7 @@ function resetProgress() {
     numberOfCookies -= resourcePrice;
     clickUpgradeIncrease += 10;
     resourcePrice *= 8;
-    resourceUpgradePrice *= 2;
+    resourceUpgradePrice += 10;
     
     progressBar == 0;
     document.getElementById("progressBar").style.width = 0 + "%";
@@ -122,26 +133,40 @@ const clickUpgradeFunction = () => {
         clickUpgradeIncrease++;
         costOfClickUpgrade *= 1.5;
         clickUpgrade.innerHTML = `Buy better tools: ${Math.round(costOfClickUpgrade)}`
-        Math.round(costOfClickUpgrade);
         updateProgress();
         x +=y
         upgradeCount++;
 
     }
 };
+const excavatorUpgradeFunction = () => {
+    if (numberOfCookies >= costOfExcavator) {
+        //odecteni ceny
+        numberOfCookies -= costOfExcavator;
+        //aktualizace poctu
+        counter.innerHTML = `MONEY: ${Math.round(numberOfCookies)}`;
+        //zvedneme pocet susenek na klik
+        ExcavatorIncrease+=10;
+        costOfExcavator *= 1.4;
+        excavatorUpgrade.innerText = `Buy excavator: ${Math.round(costOfExcavator)}`;
+
+    }
+};
+
 
 clickUpgrade.onclick = clickUpgradeFunction;
+excavatorUpgrade.onclick = excavatorUpgradeFunction;
 
 autoClicker.onclick = () => {
     if (numberOfCookies >= autoclickerPrice) {
         numberOfCookies -= autoclickerPrice;
         //aktualizace poctu
-        counter.innerHTML = `MONEY: ${numberOfCookies}`;
+        counter.innerHTML = `MONEY: ${Math.round(numberOfCookies)}`;
         //zvyseni ucinnosti
         autoclickIncrease++;
         //cena
-        autoclickerPrice *= 2;
-        autoClicker.innerText = "Buy slave: " + autoclickerPrice;
+        autoclickerPrice *= 1.5;
+        autoClicker.innerText = "Buy slave: " + Math.round(autoclickerPrice);
         //clear
         clearInterval(automatickeklikani);
         //interval
@@ -151,12 +176,38 @@ autoClicker.onclick = () => {
         }, 1000);
 
     }
+}
+Otrokarna.onclick = () => {
+    if (numberOfCookies >= lepsiOtrokCena) {
+        numberOfCookies -= lepsiOtrokCena;
+        //aktualizace poctu
+        counter.innerHTML = `MONEY: ${Math.round(numberOfCookies)}`;
+        //zvyseni ucinnosti
+        lepsiOtrok+= 10;
+        //cena
+        lepsiOtrokCena *= 1.5;
+        Otrokarna.innerText = "Buy slave: " + Math.round(lepsiOtrokCena);
+        //clear
+        clearInterval(otrok);
+        //interval
+        otrok = setInterval(() => {
+            numberOfCookies += lepsiOtrok;
+            counter.innerHTML = `MONEY: ${Math.round(numberOfCookies)}`;
+        }, 1000);
 
+    }
+
+    
 
 
 }
-
-
+function update() {
+    setInterval(() => {
+       let y = lepsiOtrok + autoclickIncrease;
+        perSec.innerHTML = `MONEY: ${y}/sec`;
+    }, 100);
+}
+update();
 
 
 
@@ -182,7 +233,7 @@ const cheats2 = () => {
 
 
 let i = 0;
-while (i<20) {
+while (i<12) {
     cheats();
     cheats2();
     i++;
